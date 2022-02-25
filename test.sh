20330ebde546a695a46739c2cb177e45cff4e92b
muhammadahmer36 <88244238+muhammadahmer36@users.noreply.github.com>
2022-02-25 23:49:05 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,43 +1,36 @@
 #!/bin/bash
 dir="/tmp/old"
-newdir="/tmp/new"
-three="3"
+backupdir="/tmp/backup"
+
 
 now="$(date +'%d-%m-%Y-%T')"
 # Build urls
-url="https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
-file="owid-covid-data.csv".$now 
+url="https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/jhu/COVID-19%20-%20Johns%20Hopkins%20University.csv"
+url1="https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/jhu/biweekly_cases.csv"
+url2="https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/jhu/biweekly_cases_per_million.csv"
+url3="https://github.com/owid/covid-19-data/blob/master/public/data/jhu/biweekly_deaths.csv"
+file="COVID-19 - Johns Hopkins University.csv".$now
+file1="biweekly_cases.csv".$now
+file2="biweekly_cases_per_million.csv".$now
+file3="biweekly_deaths.csv".$now
+
 ### Check for dir, if not found create it using the mkdir ##
 [ ! -d "$dir" ] && mkdir -p "$dir"
- 
+[ ! -d "$backupdir" ] && mkdir -p "$backupdir"
+
+
 # Now download it
 wget -qc "$url" -O "${dir}/${file}"
+wget -qc "$url1" -O "${dir}/${file1}"
+wget -qc "$url2" -O "${dir}/${file2}"
+wget -qc "$url3" -O "${dir}/${file3}"
 
 
 printf "New Directory '${dir}' recent files downloaded are below\n"
-ls -Art ${dir} | tail -n 5
+ls -Art ${dir} 
 
-cd ${dir}
-count=$(ls | wc -l)
-printf "Downloaded files count is ::-> ${count} \n\n"
+# Move the file which are three days old
 
-if [[ "${count}" -ge "${three}" ]]
-then
-
-check="1"
-printf "Moving Recent three files into New directory ${newdir} \n\n"
-mv `ls -Art ${dir} | tail -n 3` ${newdir}
-
-fi
-
-
-
-if [[ "${check}"  -eq  "1" ]]
-then
-printf "Deleting all files into old directory ${dir}\n\n"
-
-cd ${dir}
-rm -rf *.*
-fi
+find ${dir} -mtime +3 -exec mv "{}" ${backupdir};
 
 printf "Ending script"
